6fbf37b4b4109981bbcb5c8857898f690b6f30d9
Milad Khan <mildk123@gmail.com>
2023-04-28 18:05:53 +0500

    Update settings.py

--- dareecha/settings.py
+++ dareecha/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9ha8kev#c8+$45@i4ynlt!8@bz7=ow(hl!l-$#fqb(a!i_*6%&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
